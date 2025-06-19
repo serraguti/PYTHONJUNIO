49d922922c16ba2a61dfe12d53760e6a0d67047b
serraguti <pacoserranox@gmail.com>
2025-06-19 16:16:47 +0200

    arriba calc

--- python07metodos.py
+++ python07metodos.py
@@ -13,13 +13,30 @@ def saludar(nombre):
     print("Bienvenido/a a Python ", nombre)
 def despedirse(nombre, dia):
     print(f"Hasta luego {nombre} en el dia {dia}")
+#METODO DE ACCION
+def mostrarMenu():
+    print("Seleccione una opción")
+    print("1.- Convertir a mayúsculas")
+    print("2.- Convertir a minúsculas")
+#METODOS CON RETURN. LA SINTAXIS ES LA MISMA
+def convertirMayusculas(texto):
+    return texto.upper()
+def convertirMinusculas(texto):
+    return texto.lower()
 #-------------------------------------------------------------
 #CODIGO PRINCIPAL DE NUESTRO PROGRAMA MAIN
 print("Ejemplo métodos")
-saludar("Paco")
-despedirse("Paco", "Juernes")
-#LLAMADA LOS METODOS
-primerMetodo()
-segundoMetodo()
-primerMetodo()
+mostrarMenu()
+#LOS METODOS QUE DEVUELVEN VALOR DEBEN ALMACENAR EL 
+#VALOR DEVUELTO EN ALGUN SITIO/VARIABLE
+mayus = convertirMayusculas("curso de python")
+print(mayus)
+minus = convertirMinusculas("CURSo dE PyThon")
+print(minus)
+# saludar("Paco")
+# despedirse("Paco", "Juernes")
+# #LLAMADA LOS METODOS
+# primerMetodo()
+# segundoMetodo()
+# primerMetodo()
 print("Fin de programa")
---
+++ python08metodoscalculadora.py
@@ -0,0 +1,43 @@
+#DECLARACION DE LOS METODOS
+def sumarNumeros(num1, num2):
+    return num1 + num2
+def multiplicarNumeros(num1, num2):
+    return num1 * num2
+def mostrarMenu():
+    print("0.- Salir")
+    print("1.- Sumar")
+    print("2.- Multiplicar")
+    print("3.- Introducir nuevos numeros")
+    print("Seleccione una opcion")
+def getComprobarNumero():
+    print("Introduzca un numero")
+    #NECESITAMOS UN STRING PARA COMPROBAR
+    aux = input()    
+    while (aux.isdigit() == False):
+        print("Esto no es un numero")
+        print("Introduzca un numero")
+        aux = input()
+    num = int(aux)
+    return num
+#---------------------------------
+#PROGRAMA PRINCIPAL
+print("Calculadora de métodos")
+numero1 = getComprobarNumero()
+numero2 = getComprobarNumero()
+#CREAMOS UNA VARIABLE OPCION PARA ENTRAR AL BUCLE
+opcion = 1
+while (opcion != 0):
+    mostrarMenu()
+    opcion = int(input())
+    if (opcion == 1):
+        suma = sumarNumeros(numero1, numero2)
+        print(f"La suma de {numero1} y {numero2} es {suma}")
+    elif (opcion == 2):
+        multi = multiplicarNumeros(numero1, numero2)
+        print(f"{numero1} * {numero2} = {multi}")
+    elif (opcion == 3):
+        numero1 = getComprobarNumero()
+        numero2 = getComprobarNumero()
+    else:
+        print("Opcion incorrecta")
+print("Fin de programa")
